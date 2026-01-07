c018b89defb44801aec0730781e414421324262d
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2026-01-07 09:23:31 +0300

    feat(Add Group By Config): add add group by config doctype

---
+++ add_by_group/add_by_group/doctype/add_by_group_config/add_by_group_config.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Emanuel Fidelis and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Add By Group Config", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ add_by_group/add_by_group/doctype/add_by_group_config/test_add_by_group_config.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Emanuel Fidelis and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAddByGroupConfig(FrappeTestCase):
+	pass
